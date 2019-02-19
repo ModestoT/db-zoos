7a5dc5c0641e03ba8a2e8c7db41bf6a25eb6c94d
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-19 14:13:05 -0600

    configured knex to know about the sqlite3 file in the data's folder and renamed the nodemon script in package.json to be server

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/lamda.sqlite3',
+  },
+  useNullAsDefault: true, // needed for sqlite
+};
+const db = knex(knexConfig);
 
 const server = express();
 
-server.use(express.json());
 server.use(helmet());
-
+server.use(express.json());
 // endpoints here
 
 const port = 3300;
