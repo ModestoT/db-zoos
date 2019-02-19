13c12c8cad71447f35b1851340fb11eb38a53413
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-19 15:08:36 -0600

    Added a bears table to the sqlite3 file and added all CRUD opeations for the bears table

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ server.use(helmet());
 server.use(express.json());
 // endpoints here
 
+//======================================================== Zoos routes
 server.get('/api/zoos', async (req, res) => {
   try {
     const zoos = await db('zoos'); 
@@ -80,6 +81,78 @@ server.put('/api/zoos/:id', async (req, res) => {
         res.status(200).json(zoo);
       } else {
         res.status(404).json({ error: 'No zoo with that ID was found' });
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+});
+
+//========================================================== Bears routes
+
+server.get('/api/bears', async (req, res) => {
+  try {
+    const bears = await db('bears'); 
+    res.status(200).json(bears);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.get('/api/bears/:id', async (req, res) => {
+  try {
+    const bear = await db('bears').where({ id: req.params.id }).first();
+    if(bear){
+      res.status(200).json(bear);
+    } else {
+      res.status(404).json({ error: 'No bear with that ID was found' });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.post('/api/bears', async (req, res) => {
+  if(!req.body.name){
+    res.status(400).json({ error: 'Please assign a name to the bear' });
+  } else {
+    try {
+      const [id] = await db('bears').insert(req.body);
+      const bear = await db('bears').where({ id }).first();
+
+      res.status(201).json(bear);
+    } catch (error){
+      res.status(500).json(error);
+    }
+  }
+});
+
+server.delete('/api/bears/:id', async (req, res) => {
+  try {
+    const count = await db('bears').where({ id: req.params.id }).del();
+
+    if(count > 0 ){
+      res.status(204).end();
+    } else {
+      res.status(404).json({ error: 'No Bear with that ID was found' });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.put('/api/bears/:id', async (req, res) => {
+  if(!req.body.name){
+    res.status(400).json({ error: 'Please assign a name to the Bear' });
+  } else {
+    try {
+      const count = await db('bears').where({ id: req.params.id }).update(req.body);
+
+      if(count > 0 ){
+        const bear = await db('bears').where({ id: req.params.id }).first();
+        res.status(200).json(bear);
+      } else {
+        res.status(404).json({ error: 'No bear with that ID was found' });
       }
     } catch (error) {
       res.status(500).json(error);
