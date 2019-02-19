1c0130c9375a486743534f2eeb75a94054d4a8b2
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-19 14:33:49 -0600

    Wrote the end-points for getting the zoos, getting a zoo by its id and posting a zoo to the Database

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const knex = require('knex');
 const knexConfig = {
   client: 'sqlite3',
   connection: {
-    filename: './data/lamda.sqlite3',
+    filename: './data/lambda.sqlite3',
   },
   useNullAsDefault: true, // needed for sqlite
 };
@@ -16,6 +16,43 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 // endpoints here
+
+server.get('/api/zoos', async (req, res) => {
+  try {
+    const zoos = await db('zoos'); 
+    res.status(200).json(zoos);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.get('/api/zoos/:id', async (req, res) => {
+  try {
+    const zoo = await db('zoos').where({ id: req.params.id }).first();
+    if(zoo){
+      res.status(200).json(zoo);
+    } else {
+      res.status(400).json({ error: 'No zoo with that id was found' });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.post('/api/zoos', async (req, res) => {
+  if(!req.body.name){
+    res.status(400).json({ error: 'Please assign a name to the Zoo' });
+  } else {
+    try {
+      const [id] = await db('zoos').insert(req.body);
+      const zoo = await db('zoos').where({ id }).first();
+
+      res.status(201).json(zoo);
+    } catch (error){
+      res.status(500).json(error);
+    }
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
